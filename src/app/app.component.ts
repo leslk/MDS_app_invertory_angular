090f2f3d6a4886983a31df75a7c9d8adcb8fc360
leslie el_kattoussi <lesly014@hotmail.fr>
2024-02-20 09:55:02 +0100

    [add] header (with buttons that change view) & views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { EntryComponent } from './entry/entry.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, EntryComponent, InventoryComponent],
+  template: `
+    <header>
+      <nav>
+        <button><a routerLink="/entry" routerLinkActive="active" (click)="setTypeOfView('entry')">Saisie</a></button>
+        <button><a routerLink="/inventory" routerLinkActive="active" (click)="setTypeOfView('inventory')" >Inventaire</a></button>
+      </nav>
+      @if (typeOfView === "entry") {
+        <app-entry></app-entry>
+      }
+      @else {
+        <app-inventory></app-inventory>
+      }
+    
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app_inventory';
+  typeOfView = "entry";
+
+  setTypeOfView(type: string) {
+    this.typeOfView = type;
+  }
 }
